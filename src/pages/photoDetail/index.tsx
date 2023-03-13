516511ac74ab8b8b3ce71f55c818f85365b00996
xiao <1262260527@qq.com>
2023-03-13 18:05:14 +0800

    fix: update

--- src/pages/photoDetail/index.tsx
+++ src/pages/photoDetail/index.tsx
@@ -19,20 +19,47 @@ import { scrollToElement, toggleBodyOverflow } from "@/utils/common";
 
 type Mood = Record<string, { text: string; icon: string }>;
 
-// Mood Location  View
-const MoodEnum: Mood = {
-  Awesome: { text: "Awesome", icon: "icon-emoji" },
-  Good: { text: "Good", icon: "icon-emoji" },
-  Meh: { text: "Meh", icon: "icon-emoji" },
-  Bad: { text: "Bad", icon: "icon-emoji" },
-  Terrible: { text: "Terrible", icon: "icon-emoji" },
+const MoodEnum: { [name: string]: JSX.Element } = {
+  Awesome: (
+    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+      <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm141.4 389.4c-37.8 37.8-88 58.6-141.4 58.6s-103.6-20.8-141.4-58.6S48 309.4 48 256s20.8-103.6 58.6-141.4S194.6 56 248 56s103.6 20.8 141.4 58.6S448 202.6 448 256s-20.8 103.6-58.6 141.4zM328 152c-23.8 0-52.7 29.3-56 71.4-.7 8.6 10.8 11.9 14.9 4.5l9.5-17c7.7-13.7 19.2-21.6 31.5-21.6s23.8 7.9 31.5 21.6l9.5 17c4.1 7.4 15.6 4 14.9-4.5-3.1-42.1-32-71.4-55.8-71.4zm-201 75.9l9.5-17c7.7-13.7 19.2-21.6 31.5-21.6s23.8 7.9 31.5 21.6l9.5 17c4.1 7.4 15.6 4 14.9-4.5-3.3-42.1-32.2-71.4-56-71.4s-52.7 29.3-56 71.4c-.6 8.5 10.9 11.9 15.1 4.5zM362.4 288H133.6c-8.2 0-14.5 7-13.5 15 7.5 59.2 58.9 105 121.1 105h13.6c62.2 0 113.6-45.8 121.1-105 1-8-5.3-15-13.5-15z" />
+    </svg>
+  ),
+  Good: (
+    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+      <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 448c-110.3 0-200-89.7-200-200S137.7 56 248 56s200 89.7 200 200-89.7 200-200 200zm-80-216c17.7 0 32-14.3 32-32s-14.3-32-32-32-32 14.3-32 32 14.3 32 32 32zm160 0c17.7 0 32-14.3 32-32s-14.3-32-32-32-32 14.3-32 32 14.3 32 32 32zm4 72.6c-20.8 25-51.5 39.4-84 39.4s-63.2-14.3-84-39.4c-8.5-10.2-23.7-11.5-33.8-3.1-10.2 8.5-11.5 23.6-3.1 33.8 30 36 74.1 56.6 120.9 56.6s90.9-20.6 120.9-56.6c8.5-10.2 7.1-25.3-3.1-33.8-10.1-8.4-25.3-7.1-33.8 3.1z" />
+    </svg>
+  ),
+  Meh: (
+    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+      <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 448c-110.3 0-200-89.7-200-200S137.7 56 248 56s200 89.7 200 200-89.7 200-200 200zm-80-216c17.7 0 32-14.3 32-32s-14.3-32-32-32-32 14.3-32 32 14.3 32 32 32zm160-64c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm8 144H160c-13.2 0-24 10.8-24 24s10.8 24 24 24h176c13.2 0 24-10.8 24-24s-10.8-24-24-24z" />
+    </svg>
+  ),
+  Bad: (
+    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+      <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 448c-110.3 0-200-89.7-200-200S137.7 56 248 56s200 89.7 200 200-89.7 200-200 200zm-80-216c17.7 0 32-14.3 32-32s-14.3-32-32-32-32 14.3-32 32 14.3 32 32 32zm160-64c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm-80 128c-40.2 0-78 17.7-103.8 48.6-8.5 10.2-7.1 25.3 3.1 33.8 10.2 8.4 25.3 7.1 33.8-3.1 16.6-19.9 41-31.4 66.9-31.4s50.3 11.4 66.9 31.4c8.1 9.7 23.1 11.9 33.8 3.1 10.2-8.5 11.5-23.6 3.1-33.8C326 321.7 288.2 304 248 304z" />
+    </svg>
+  ),
+  Terrible: (
+    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+      <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 448c-110.3 0-200-89.7-200-200S137.7 56 248 56s200 89.7 200 200-89.7 200-200 200zm8-152c-13.2 0-24 10.8-24 24s10.8 24 24 24c23.8 0 46.3 10.5 61.6 28.8 8.1 9.8 23.2 11.9 33.8 3.1 10.2-8.5 11.6-23.6 3.1-33.8C330 320.8 294.1 304 256 304zm-88-64c17.7 0 32-14.3 32-32s-14.3-32-32-32-32 14.3-32 32 14.3 32 32 32zm160-64c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm-165.6 98.8C151 290.1 126 325.4 126 342.9c0 22.7 18.8 41.1 42 41.1s42-18.4 42-41.1c0-17.5-25-52.8-36.4-68.1-2.8-3.7-8.4-3.7-11.2 0z" />
+    </svg>
+  ),
 };
+// Mood Location  View
+// const MoodEnum: Mood = {
+//   Awesome: { text: "Awesome", icon: "icon-emoji" },
+//   Good: { text: "Good", icon: "icon-emoji" },
+//   Meh: { text: "Meh", icon: "icon-emoji" },
+//   Bad: { text: "Bad", icon: "icon-emoji" },
+//   Terrible: { text: "Terrible", icon: "icon-emoji" },
+// };
 
 const Index = () => {
   const [routeParams] = useSearchParams();
   const routeLocation = useLocation();
 
-  const id = routeParams.get('id');
+  const id = routeParams.get("id");
   const { t } = useTranslation();
   const [curPhotoInfo, setCurPhotoInfo] = useState<PhotoList>(); // 当前图片信息
   const [photoList, setPhotoList] = useState<PhotoList[]>(); // 图片列表
@@ -314,9 +341,14 @@ const Index = () => {
 
             {/* 图片扩展信息 */}
             <div className="flex flex-wrap -mx-8 text-12 leading-sm lg:text-14 lg:leading-md">
-              <div className={classnames("items-center px-8", curPhotoInfo?.mood ? "flex" : "hidden")}>
+              {/* <div className={classnames("items-center px-8", curPhotoInfo?.mood ? "flex" : "hidden")}>
                 <Icon className={MoodEnum?.[curPhotoInfo?.mood!]?.icon} addClassName="text-14 lg:text-16 mr-4" />
                 <span>{MoodEnum?.[curPhotoInfo?.mood!]?.text}</span>
+              </div> */}
+              <div className={classnames("items-center px-8", curPhotoInfo?.mood ? "flex" : "hidden")}>
+                {/* <Icon className={?.icon} addClassName="text-14 lg:text-16 mr-4" /> */}
+                <span className="text-14 lg:text-16 mr-4">{MoodEnum?.[curPhotoInfo?.mood!]}</span>
+                <span>{curPhotoInfo?.mood!}</span>
               </div>
 
               <div className={classNames("items-center px-8", curPhotoInfo?.place ? "flex" : "hidden")}>
